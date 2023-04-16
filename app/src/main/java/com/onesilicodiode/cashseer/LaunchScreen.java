d60fe324d132738f13d389dca8e2d234fc89ecd0
Spandan Saxena <spandansaxena@gmail.com>
2023-04-16 16:20:22 +0530

    Added Tensorflow Project

--- app/src/main/java/com/onesilicodiode/cashseer/LaunchScreen.java
+++ app/src/main/java/com/onesilicodiode/cashseer/LaunchScreen.java
@@ -2,13 +2,45 @@ package com.onesilicodiode.cashseer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 
+@SuppressLint("CustomSplashScreen")
 public class LaunchScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch_screen);
+        fireLaunch();
+    }
+
+    private void fireLaunch() {
+        int splash_screen_short = 2000;
+        new Handler().postDelayed(() -> {
+            vibrateDevice();
+            Intent toLanding = new Intent(this, MainActivity.class);
+            startActivity(toLanding);
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            finish();
+        }, splash_screen_short);
+
+    }
+
+    private void vibrateDevice() {
+            Vibrator v3 = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                v3.vibrate(VibrationEffect.createOneShot(28, VibrationEffect.DEFAULT_AMPLITUDE));
+            } else {
+                //deprecated in API 26
+                v3.vibrate(25);
+            }
     }
 }
